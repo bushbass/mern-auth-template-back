43b6b088c1808c9c2f4a8bf525b142a9b52d3cc9
Alex Nielsen <alex@bigal.org>
2020-11-27 23:13:29 -0500

    finished lesson 8

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -22,4 +22,19 @@ router.post('/', auth, async (req, res) => {
   }
 });
 
+router.get('/all', auth, async (req, res) => {
+  const todos = await Todo.find({ userId: req.user });
+  res.json(todos);
+});
+
+router.delete('/:id', auth, async (req, res) => {
+  const todo = await Todo.findOne({ userId: req.user, _id: req.params.id });
+  if (!todo)
+    return res
+      .status(400)
+      .json({ msg: 'No todo found with this id that belongs to current user' });
+  const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+  res.json(deletedTodo);
+});
+
 module.exports = router;
